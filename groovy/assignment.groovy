dea7d98a75db6054ef11d00eabc66dd22385cef2
RX-PrateekM <prateek.maheshwari@rxlogix.com>
2022-02-28 10:10:45 +0530

    add:assignment till Q5

---
+++ groovy/assignment.groovy
@@ -0,0 +1,36 @@
+// Question 1 , 2 and 4
+
+// Employee emp = new Employee(name:"Prateek",age:20,gender:'Male',address:"Noida",
+// empId:120,company:"RxLogix",salary:null);
+// class Person{
+//   def name,age,gender,address;
+// }
+
+// class Employee extends Person{
+//   def empId,company,salary;
+
+//     @Override
+//     public String toString(){
+//         return """${this.name} is a ${this.gender} aged ${this.age} who lives in ${this.address}. He works
+//         for ${this.company} with employee id ${this.empId} and draws ${this.salary} lots of money. """
+//     }
+
+// }
+
+
+// println "${emp.name} is aged ${emp.getAge()}"
+// print "${emp}";
+
+
+// Question 3
+
+// for (i in 0..3) {
+//     for(j in 1..(2**i)){
+//         print("*");
+//     }
+//     print("\n");
+// }
+
+// Question 5 -> Incomplete
+
+// Question 6 
